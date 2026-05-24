78b98eadab37e6948044b15ced6b6f8172f9023a
leonardespi <leonardespicass@gmail.com>
2026-05-23 19:11:15 -0600

    content: remove service phones and physical address from site

--- src/config/site.ts
+++ src/config/site.ts
@@ -9,11 +9,8 @@ export const SITE = {
     emailSales:      'jherrera.ultrec@gmail.com',
     emailService:    'Servicios.ultrec@gmail.com',
     phoneSales:      '+52 55 1139 2294',
-    phoneService1:   '+52 55 4968 2196',
-    phoneService2:   '+52 55 1191 0216',
     whatsapp:        '5215511392294',
     whatsappMessage: 'Bienvenido a ULTREC. ¿En qué podemos servirte?',
-    address:         'Transvaal 531, Col. 1ro de Mayo, Venustiano Carranza, CDMX 15440',
     hours:           'Lunes a Viernes, 9:00 am – 6:00 pm',
   },
 
